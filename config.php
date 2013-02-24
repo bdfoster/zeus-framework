2814b81b7b16aebdee7ecb9041ba1d8cb4f6bb3a
Brian Foster <bdfoster@endigotech.com>
2013-02-24 16:26:50 -0500

    add config.php/index.php, move definitions to config.php, includes in index.php (again)

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. 
+ */
+ 
+// ini_set('display_errors', 'On');
+// error_reporting(E_ALL | E_STRICT);
+define('ROOT', __DIR__ . '/..');
+define('ZEUS', ROOT . 'zeus.php');
